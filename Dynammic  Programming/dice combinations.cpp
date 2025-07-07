6666631b80953fca5c30f4e8e79e4bdb41546724
akshatt-15 <113547720+akshatt-15@users.noreply.github.com>
2025-07-07 11:39:08 +0530

    Add files via upload

---
+++ Dynammic  Programming/dice combinations.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long  long MOD=1e9+7;
+    int n;
+    cin>>n;
+    vector<int>dp(n+1);
+    dp[0]=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=6;j++)
+        {
+            if(i>=j)
+            {
+                dp[i]=(dp[i]+dp[i-j])%MOD;
+            }
+        }
+
+    }
+    cout<<dp[n];
+    return 0;
+}
